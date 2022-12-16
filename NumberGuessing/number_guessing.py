a7d539f3113a3e12c67154649d7f6b8a36585f8b
Lucas Galarani <Lucasgalaranidc@gmail.com>
2022-12-16 14:05:12 -0300

    A game for guess numbers

--- NumberGuessing/number_guessing.py
+++ NumberGuessing/number_guessing.py
@@ -10,3 +10,21 @@ while flag:
         flag = False
     else:
         print('Invalid input! Try Again!')
+
+secret = random.randint(1,num)
+
+guess = None
+count = 1
+
+while guess != secret:
+    guess = input('Please type a number between 1 and ' + str(num) + ": ")
+    if guess.isdigit():
+        guess = int(guess)
+        
+    if guess == secret:
+        print('You got it')
+    else:
+        print('Try again!')
+        count += 1
+        
+print('It took you', count, 'guesses!')
