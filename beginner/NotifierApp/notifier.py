1cc68573c2516bf147007c9b9d3d97cfef7873ef
Lucas Galarani <Lucasgalaranidc@gmail.com>
2022-12-30 20:50:13 -0300

    new projects

--- beginner/NotifierApp/notifier.py
+++ beginner/NotifierApp/notifier.py
@@ -3,8 +3,8 @@ from plyer import notification
 
 while True:
     notification.notify(
-       title = 'Hello User!'
-       message = 'Wake up and drink a cup of Water'
+       title = "Hello User"
+       message = "Wake up and drink a cup of Water"
        timeout = 10
     )
 time.sleep(10)
---
+++ beginner/StoryGenerator/generator.py
@@ -0,0 +1,12 @@
+import random
+
+print('Hello User,today you make a diferent storys.')
+print('The storys are doing in random,so you dont define nothing.')
+print()
+
+readername = input('Whats is your name: ')
+print('Hello' ' ' + readername)
+
+names = ['Lucas', 'Kevin', 'Ana', 'Gus', 'Meleny', 'John']
+places = ['Mall', 'SuperMarket', 'Restaraunt', 'Starbucks', 'Hospital', 'Party']
+quests = []
